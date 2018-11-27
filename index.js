948b73022e15ff3fdbed9893e773198d26c2aefc
phengemuhle <phengemuhle@gmail.com>
2018-11-27 12:52:42 -0700

    completed queries

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const queries = require('./queries')
 
 
 app.get('/', (req,res) => {
-    res.send('yay')
+    queries.listAll().then(students => res.json(students))
 })
 
 app.listen(port, () => {
