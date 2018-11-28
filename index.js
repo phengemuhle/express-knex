d7652a367f58e5de7e973cce5f2969b1bb4a25af
phengemuhle <phengemuhle@gmail.com>
2018-11-28 10:00:38 -0700

    added new queries for id

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const queries = require('./queries')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req,res) => {
     queries.listAll().then(students => res.send(students))
+})
+app.get('/:id', (req,res) => {
+    var { id } = req.params
+    queries.getById(id).then(guy => res.send(guy))
 })
 
 app.listen(port, () => {
--- queries.js
+++ queries.js
@@ -1,7 +1,11 @@
 const database = require('./database-connection')
 
+
 module.exports = {
     listAll(){
         return database('students')
+    },
+    getById(id){
+        return database('students').where({ id: id }).first()
     }
   }
